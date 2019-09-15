341da70c9c06f551672ce38ce3668fdd991a3ffb
Pongdanai <pongdanai.ni@ku.th>
2019-09-15 20:45:54 +0700

    initialize and add adapter pattern

---
+++ src/DuckSimulator.java
@@ -0,0 +1,22 @@
+public class DuckSimulator {
+    public static void main(String[] args) {
+        DuckSimulator simulator = new DuckSimulator();
+        simulator.simulate();
+    }
+    void simulate() {
+        Quackable mallardDuck = new MallardDuck();
+        Quackable redheadDuck = new RedheadDuck();
+        Quackable rubberDuck = new RubberDuck();
+        Quackable gooseDuck = new GooseAdapter(new Goose());
+        Quackable pigeonDuck = new PigeonAdapter(new Pigeon());
+        System.out.println("\nDuck Simulator");
+        simulate(mallardDuck);
+        simulate(redheadDuck);
+        simulate(rubberDuck);
+        simulate(gooseDuck);
+        simulate(pigeonDuck);
+    }
+    void simulate(Quackable duck) {
+        duck.quack();
+    }
+}
